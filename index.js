5546dab079e8f77a25dbcaa69143ad9eb2d8303c
Shahzaib <69957799+Shahzaib758@users.noreply.github.com>
2023-03-14 22:27:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const server = app.listen(process.env.PORT || 5000, () => {
 
 const io = require("socket.io")(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
     },
 });
 
